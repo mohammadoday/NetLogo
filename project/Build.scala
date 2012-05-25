9948226e32ec60915a5bb9fa4cc02a0e98d26626
Seth Tisue <seth@tisue.net>
2012-05-25 11:16:55 -0400

    reindent

--- project/Build.scala
+++ project/Build.scala
@@ -7,12 +7,12 @@ object NetLogoBuild extends Build {
       id = "NetLogo",
       base = file("."),
       settings = Defaults.defaultSettings ++ Seq(
-      sourceGenerators in Compile <+=
-        (javaSource in Compile, baseDirectory, streams) map {
-          (dir, base, s) =>
-            Seq(Autogen.events(s.log.info(_), base, dir, "window"),
-                Autogen.events(s.log.info(_), base, dir, "app"),
-                Autogen.flex(s.log.info(_), base, dir, "agent", "ImportLexer"),
-                Autogen.flex(s.log.info(_), base, dir, "lex", "TokenLexer"))}
-      ))
+        sourceGenerators in Compile <+=
+          (javaSource in Compile, baseDirectory, streams) map {
+            (dir, base, s) =>
+              Seq(Autogen.events(s.log.info(_), base, dir, "window"),
+                  Autogen.events(s.log.info(_), base, dir, "app"),
+                  Autogen.flex(s.log.info(_), base, dir, "agent", "ImportLexer"),
+                  Autogen.flex(s.log.info(_), base, dir, "lex", "TokenLexer"))}
+        ))
 }
