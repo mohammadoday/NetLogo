d860b96e5ec4f8908da8d56f3bdbdb0c341a0eb3
Seth Tisue <seth@tisue.net>
2012-05-25 20:49:26 -0400

    make package task copy it to NetLogo.jar when done

--- project/Build.scala
+++ project/Build.scala
@@ -18,7 +18,11 @@ object NetLogoBuild extends Build {
               "Class-Path", classpath.files
                 .map(f => "lib/" + f.getName)
                 .filter(_.endsWith(".jar"))
-                .mkString(" ")))}
+                .mkString(" ")))},
+        packageBin in Compile ~= { jar =>
+          IO.copyFile(jar, file(".") / "NetLogo.jar")
+          jar
+        }
       )
     )
 }
