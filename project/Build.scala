c02f46d89c633e9a1f87883bb2202d145ad01e39
Seth Tisue <seth@tisue.net>
2012-05-29 17:08:19 -0400

    get extensions building (for the most part)

--- project/Build.scala
+++ project/Build.scala
@@ -21,8 +21,12 @@ object NetLogoBuild extends Build {
                 .mkString(" ")))},
         packageBin in Compile ~= { jar =>
           IO.copyFile(jar, file(".") / "NetLogo.jar")
+          // temporary hack until we get ProGuard going to shrink the lite jar - ST 5/25/12
+          IO.download(new URL("http://ccl.northwestern.edu/netlogo/5.0.1/NetLogoLite.jar"),
+                      file(".") / "NetLogoLite.jar")
           jar
-        }
+        },
+        Extensions.extensionsTask
       )
     )
 }
--- project/Extensions.scala
+++ project/Extensions.scala
@@ -2,25 +2,31 @@ import java.io.File
 import sbt._
 import Keys._
 
-trait Extensions {
+object Extensions {
 
-  val extensions =
-    baseDirectory map { base =>
+  val extensions = TaskKey[Seq[File]](
+    "extensions", "builds extensions")
+
+  val extensionsTask =
+    extensions <<= (baseDirectory, scalaInstance, streams) map { (base, scala, s) =>
+      "git submodule --quiet update --init" ! s.log
       val isDirectory = new java.io.FileFilter {
         override def accept(f: File) = f.isDirectory
       }
-      val dirs = IO.listFiles(isDirectory)(base / "extensions")
-      dirs.flatMap(buildExtension)
-    }
+      val dirs = IO.listFiles(isDirectory)(base / "extensions").toSeq
+      dirs.map(buildExtension(_, scala.libraryJar, s.log))
+    } dependsOn(packageBin in Compile)
 
-  private def buildExtension(dir: File): Seq[File] = Nil
+  private def buildExtension(dir: File, scalaLibrary: File, log: Logger): File = {
+    log.info("building extension: " + dir.getName)
+    val jar = new File(dir, dir.getName + ".jar")
+    println(scalaLibrary.getPath)
+    Process(List("make", "-s", jar.getName), dir,
+            "SCALA_JAR" -> scalaLibrary.getPath) ! log
+    jar
+  }
 
 }
-    
-/*
-# most of them use NetLogoLite.jar, but the profiler extension uses NetLogo.jar - ST 5/11/11
-$(EXTENSIONS) $(EXTENSIONS_PACK200): | NetLogo.jar NetLogoLite.jar
-	git submodule update --init
-	@echo "@@@ building" $(notdir $@)
-	cd $(dir $@); JAVA_HOME=$(JAVA_HOME) SCALA_JAR=../../$(SCALA_JAR_BASE) make -s $(notdir $@)
-*/
+
+// cd $(dir $@); JAVA_HOME=$(JAVA_HOME) SCALA_JAR=../../$(SCALA_JAR_BASE) make -s $(notdir $@)
+
