462b7d9dbe2777e8d0e59f7a91bd47708076d1c7
Seth Tisue <seth@tisue.net>
2012-05-30 22:16:49 -0400

    get fast:test and slow:test working

--- nightly.sh
+++ nightly.sh
@@ -16,8 +16,14 @@ mkdir -p tmp/nightly
 # maybe there's an easier way, than I've done it below, I don't know. 
 # I suck at shell scripting - ST 2/15/11
 git submodule update --init
-bin/xsbt extensions test 2>&1 | tee tmp/nightly/0-test.txt
-if [ ${PIPESTATUS[0]} -ne 0 ] ; then echo "*** FAILED: test"; exit 1; fi
-echo "*** done: test"
+bin/xsbt extensions
+
+bin/xsbt fast:test 2>&1 | tee tmp/nightly/0-fast-test.txt
+if [ ${PIPESTATUS[0]} -ne 0 ] ; then echo "*** FAILED: fast:test"; exit 1; fi
+echo "*** done: fast:test"
+
+bin/xsbt slow:test 2>&1 | tee tmp/nightly/1-slow-test.txt
+if [ ${PIPESTATUS[0]} -ne 0 ] ; then echo "*** FAILED: slow:test"; exit 1; fi
+echo "*** done: test-slow"
 
 echo "****** all done!"
--- project/Testing.scala
+++ project/Testing.scala
@@ -12,11 +12,18 @@ object Testing {
     inConfig(FastTest)(Defaults.testTasks) ++
     inConfig(SlowTest)(Defaults.testTasks) ++
     Seq(
-      testOptions in FastTest := Seq(Tests.Filter(fastFilter)),
-      testOptions in SlowTest := Seq(Tests.Filter(slowFilter))
+      testOptions in FastTest <<= (fullClasspath in Test) map { path =>
+        Seq(Tests.Filter(fastFilter(path, _))) },
+      testOptions in SlowTest <<= (fullClasspath in Test) map { path =>
+        Seq(Tests.Filter(slowFilter(path, _))) }
     )
 
-  def fastFilter(name: String): Boolean = false
-  def slowFilter(name: String): Boolean = true
+  def fastFilter(path: Classpath, name: String): Boolean = !slowFilter(path, name)
+  def slowFilter(path: Classpath, name: String): Boolean = {
+    val jars = path.files.map(_.asURL).toArray[java.net.URL]
+    val loader = new java.net.URLClassLoader(jars, getClass.getClassLoader)
+    def clazz(name: String) = Class.forName(name, false, loader)
+    clazz("org.nlogo.util.SlowTest").isAssignableFrom(clazz(name))
+  }
 
 }
