cf70344007881799d52cad91aa0f4edff01aa2e0
Seth Tisue <seth@tisue.net>
2012-05-30 21:30:11 -0400

    refactor slightly

--- project/Build.scala
+++ project/Build.scala
@@ -2,32 +2,34 @@ import sbt._
 import Keys._
 
 object NetLogoBuild extends Build {
+
   lazy val root =
-    Project(
-      id = "NetLogo",
-      base = file("."),
-      settings = Defaults.defaultSettings ++ Seq(
-        unmanagedResourceDirectories in Compile <+=
-          baseDirectory { _ / "resources" },
-        sourceGenerators in Compile <+= Autogen.sourceGeneratorTask,
-        resourceGenerators in Compile <+= I18n.resourceGeneratorTask,
-        mainClass in (Compile, packageBin) :=
-          Some("org.nlogo.app.App"),
-        packageOptions <+= dependencyClasspath in Runtime map {
-          classpath =>
-            Package.ManifestAttributes((
-              "Class-Path", classpath.files
-                .map(f => "lib/" + f.getName)
-                .filter(_.endsWith(".jar"))
-                .mkString(" ")))},
-        packageBin in Compile ~= { jar =>
-          IO.copyFile(jar, file(".") / "NetLogo.jar")
-          // temporary hack until we get ProGuard going to shrink the lite jar - ST 5/25/12
-          IO.download(new URL("http://ccl.northwestern.edu/netlogo/5.0.1/NetLogoLite.jar"),
-                      file(".") / "NetLogoLite.jar")
-          jar
-        },
-        Extensions.extensionsTask
-      )
-    )
+    Project(id = "NetLogo", base = file("."))
+      .settings(Defaults.defaultSettings ++
+                mySettings: _*)
+
+  lazy val mySettings = Seq(
+    unmanagedResourceDirectories in Compile <+=
+      baseDirectory { _ / "resources" },
+    sourceGenerators in Compile <+= Autogen.sourceGeneratorTask,
+    resourceGenerators in Compile <+= I18n.resourceGeneratorTask,
+    mainClass in (Compile, packageBin) :=
+      Some("org.nlogo.app.App"),
+    packageOptions <+= dependencyClasspath in Runtime map {
+      classpath =>
+        Package.ManifestAttributes((
+          "Class-Path", classpath.files
+            .map(f => "lib/" + f.getName)
+            .filter(_.endsWith(".jar"))
+            .mkString(" ")))},
+    packageBin in Compile ~= { jar =>
+      IO.copyFile(jar, file(".") / "NetLogo.jar")
+      // temporary hack until we get ProGuard going to shrink the lite jar - ST 5/25/12
+      IO.download(new URL("http://ccl.northwestern.edu/netlogo/5.0.1/NetLogoLite.jar"),
+                  file(".") / "NetLogoLite.jar")
+      jar
+    },
+    Extensions.extensionsTask
+  )
+
 }
