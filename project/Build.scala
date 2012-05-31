598e9bd8ccc4e7383712e1d76233df5feae813d6
Seth Tisue <seth@tisue.net>
2012-05-30 21:38:43 -0400

    refactor

--- project/Build.scala
+++ project/Build.scala
@@ -8,6 +8,7 @@ object NetLogoBuild extends Build {
       .configs(Testing.configs: _*)
       .settings(Defaults.defaultSettings ++
                 Testing.settings ++
+                Packaging.settings ++
                 moreSettings: _*)
 
   lazy val moreSettings = Seq(
@@ -17,20 +18,6 @@ object NetLogoBuild extends Build {
     resourceGenerators in Compile <+= I18n.resourceGeneratorTask,
     mainClass in (Compile, packageBin) :=
       Some("org.nlogo.app.App"),
-    packageOptions <+= dependencyClasspath in Runtime map {
-      classpath =>
-        Package.ManifestAttributes((
-          "Class-Path", classpath.files
-            .map(f => "lib/" + f.getName)
-            .filter(_.endsWith(".jar"))
-            .mkString(" ")))},
-    packageBin in Compile ~= { jar =>
-      IO.copyFile(jar, file(".") / "NetLogo.jar")
-      // temporary hack until we get ProGuard going to shrink the lite jar - ST 5/25/12
-      IO.download(new URL("http://ccl.northwestern.edu/netlogo/5.0.1/NetLogoLite.jar"),
-                  file(".") / "NetLogoLite.jar")
-      jar
-    },
     Extensions.extensionsTask
   )
 
